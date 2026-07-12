04111651c6e6d22f96b5010a233b204ec4d6a87c
David Gaspar <davidgaspar.dev@gmail.com>
2026-07-12 18:48:25 -0300

    test: Add integration tests to database/repository

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksDaoTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/data/source/local/TasksDaoTest.kt
@@ -1,4 +1,53 @@
-import org.junit.Assert.*
+package com.example.android.architecture.blueprints.todoapp.data.source.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.`is`
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
 class TasksDaoTest {
-  
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: ToDoDatabase
+
+    @Before
+    fun initDb() {
+        database = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            ToDoDatabase::class.java
+        ).build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun insertTaskAndGetById() = runTest {
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        val loaded = database.taskDao().getTaskById(task.id)
+
+        assertThat<Task>(loaded as Task, notNullValue())
+        assertThat(loaded.id, `is`(task.id))
+        assertThat(loaded.title, `is`(task.title))
+        assertThat(loaded.description, `is`(task.description))
+        assertThat(loaded.isCompleted, `is`(task.isCompleted))
+    }
 }
